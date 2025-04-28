f75f1bac91ed571e0bec2165dfdc90780fea85fe
Dipok Kumar Ray <dipok7316@gmail.com>
2025-04-28 09:02:33 +0600

    Github SignIn/SignOut  PopUP

--- src/Components/Log-out.jsx
+++ src/Components/Log-out.jsx
@@ -1,14 +1,27 @@
-import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+import { GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
 import React, { useState } from 'react';
 import { auth } from '../Firebase/Firebase.init2';
 
 
 const LogOut = () => {
 
-    const [ user, setUser] = useState(null)
-
-    const provider = new GoogleAuthProvider()
     
+    const [ user, setUser] = useState(null)
+    
+    const provider = new GoogleAuthProvider()
+    const githubProvider = new GithubAuthProvider();
+    
+    const handleSignInGithub = () => {
+        signInWithPopup(auth, githubProvider)
+        .then(result => {
+            console.log(result);
+            setUser(result.user)
+        })
+       .catch(error => {
+        console.log(error);
+       })
+    }
+
     const handleGoogleLogin = () => {
         console.log('Please Google Login !!!');
         signInWithPopup(auth, provider)
@@ -43,8 +56,11 @@ const LogOut = () => {
             <button onClick={handleGoogleLogOut}>Log Out</button> */}
 
             {
-                user ?   <button onClick={handleGoogleLogOut}>Log Out</button>
-                :  <button onClick={handleGoogleLogin}>Login</button>
+                user ?   <button onClick={handleGoogleLogOut}>Sign Out</button>
+                : <>
+                 <button onClick={handleGoogleLogin}>Sign in with Google</button>
+                <button onClick={handleSignInGithub}>Sign In With Github</button>
+                </>
 
             }
 
@@ -53,6 +69,7 @@ const LogOut = () => {
             user &&  <div>
             <h3>{user.displayName}</h3>
             <p>{user.email}</p>
+            <img src={user.photoURL} alt="" />
          
         </div>
            }
